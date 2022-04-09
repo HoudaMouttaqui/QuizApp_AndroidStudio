e3a106f414b82c08b92d7e7a626071bc2c906b65
HoudaMouttaqui <houda.mtq2@gmail.com>
2022-04-09 15:35:07 +0000

    Quiz_App

---
+++ app/src/main/java/com/mouttaqui/quizapp_o41/Quiz_topic.java
@@ -0,0 +1,14 @@
+package com.mouttaqui.quizapp_o41;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Quiz_topic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_topic);
+    }
+}
